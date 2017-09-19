450685981bf2682ccd7e8402bd3c10845347b288
tks.asahina <tks.asahina@gmail.com>
2017-09-19 21:59:35 +0900

    created customized value slider

--- src/widgets/ValueSlider/styles.js
+++ src/widgets/ValueSlider/styles.js
@@ -5,7 +5,7 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    flexDirection: 'row',
+    flexDirection: 'column',
   },
   value: {
     width: 40,
