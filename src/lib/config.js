adda1f08ee38de2af7342eab74e8f54f71f5e258
tks.asahina <tks.asahina@gmail.com>
2017-09-25 11:43:47 +0900

    added firebase initialization

---
+++ src/lib/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	//Initialize Firebase
+	firebaseConfig: {
+	apiKey: "AIzaSyATJOCv1PQv1_HU2tyZN6SfgQuakDLE_w",
+	authDomain: "postcraft-e3a32.firebaseapp.com",
+	databaseURL: "https://postcraft-e3a32.firebaseio.com",
+	projectID: "postcraft-e3a32",
+	storageBucket: ""
+	messagingSenderID: "314883363333",
+	}
+}
