5c0609cee467f4fe9550bf5f526e01d2537b1d22
tks.asahina <tks.asahina@gmail.com>
2017-10-16 02:11:31 +0900

    disabled swipe back action
    - disabled swipe back action on login, signup, craft shop screens

--- src/index.js
+++ src/index.js
@@ -58,15 +58,15 @@ export default class postcraft extends Component {
             <Router createReducer={reducerCreate}>
                 <Scene key="root">
                 <Scene key="onboarding" hideNavBar={true} component={OnBoarding} title="OnBoarding" initial />
-                <Scene key="login" hideNavBar={true} component={Login} title="Login" />
-                <Scene key="signup" hideNavBar={true} component={Signup} title="Signup" />
+                <Scene key="login" hideNavBar={true} component={Login} title="Login" type="replace" />
+                <Scene key="signup" hideNavBar={true} component={Signup} title="Signup" type="replace" />
                 <Scene key="uploadlogo" hideNavBar={true} component={UploadLogo} title="UploadLogo" />
                 <Scene key="choosebusinesstype" hideNavBar={true} component={ChooseBusinessType} title="ChooseBusinessType" />
                 <Scene key="profile" hideNavBar={true} component={Profile} title="Profile" />
                 <Scene key="profilepreview" hideNavBar={true} component={ProfilePreview} title="ProfilePreview" />
                 <Scene key="postfeed" hideNavBar={true} component={PostFeed} title="PostFeed" />
                 <Scene key="viewpost" hideNavBar={true} component={ViewPost} title="ViewPost" />
-                <Scene key="craftshop" hideNavBar={true} component={CraftShop} title="CraftShop" />
+                <Scene key="craftshop" hideNavBar={true} component={CraftShop} title="CraftShop" type="replace" />
                 <Scene key="filter" hideNavBar={true} component={Filter} title="Filter" />
                 <Scene key="cropmask" hideNavBar={true} component={CropMask} title="CropMask" />
                 <Scene key="drag" hideNavBar={true} component={Drag} title="Drag" />
